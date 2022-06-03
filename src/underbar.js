63fef8120a832485bf108b1516759918c71ffa8a
Cameron Hirsh <camjhirsh@gmail.com>
2022-06-02 21:14:31 -0700

    complete through some

--- src/underbar.js
+++ src/underbar.js
@@ -223,12 +223,33 @@
   // Determine whether all of the elements match a truth test.
   _.every = function(collection, iterator) {
     // TIP: Try re-using reduce() here.
+    if (!iterator) {
+      iterator = _.identity;
+    }
+    return _.reduce(collection, function(trueSoFar, item) {
+      if (!trueSoFar) {
+        return false;
+      }
+      return (iterator(item)) ? true : false;
+    }, true);
   };
 
   // Determine whether any of the elements pass a truth test. If no iterator is
   // provided, provide a default one
   _.some = function(collection, iterator) {
     // TIP: There's a very clever way to re-use every() here.
+
+    if (!iterator) {
+      iterator = _.identity;
+    }
+
+    return _.reduce(collection, function(anyPassed, item) {
+      if (anyPassed) {
+        return true;
+      }
+      return iterator(item) ? true : false;
+    }, false);
+
   };
 
 
