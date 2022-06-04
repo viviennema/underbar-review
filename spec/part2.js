e443463c2523e3d310bc55470fd10df99a694b81
Cameron Hirsh <camjhirsh@gmail.com>
2022-06-03 18:26:59 -0700

    "complete through memoize"

--- spec/part2.js
+++ spec/part2.js
@@ -189,7 +189,11 @@
       it('should override properties found on the destination', function() {
         // Replace this line with an `expect` statement that tests
         // the behavior described by the `it` string
-        throw new Error('This test is missing.');
+        var destination = { x: 'x' };
+        var source = { x: 'b' };
+        var extended = _.extend(destination, source);
+
+        expect(extended.x).to.equal('b');
       });
 
       it('should not override properties not found in the source', function() {
@@ -267,7 +271,12 @@
       it('should copy any property whose key is not already set on the target', function() {
         // Replace this line with an `expect` statement that tests
         // the behavior described by the `it` string
-        throw new Error('This test is missing.');
+        var destination = { a: 10 };
+        var source = { b: 1 };
+
+        _.defaults(destination, source);
+
+        expect(destination.b).to.equal(1);
       });
 
       it('should not copy a property if that key is already set on the target', function() {
--- src/underbar.js
+++ src/underbar.js
@@ -272,11 +272,25 @@
   //     bla: "even more stuff"
   //   }); // obj1 now contains key1, key2, key3 and bla
   _.extend = function(obj) {
+    for (var i = 1; i < arguments.length; i++) {
+      for (var key in arguments[i]) {
+        obj[key] = arguments[i][key];
+      }
+    }
+    return obj;
   };
 
   // Like extend, but doesn't ever overwrite a key that already
   // exists in obj
   _.defaults = function(obj) {
+    for (var i = 1; i < arguments.length; i++) {
+      for (var key in arguments[i]) {
+        if (!obj.hasOwnProperty(key) ) {
+          obj[key] = arguments[i][key];
+        }
+      }
+    }
+    return obj;
   };
 
 
